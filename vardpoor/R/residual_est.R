dff97a43311ba6d4796d608f9024809efa4156a5
juris.breidaks@gmail.com <juris.breidaks@gmail.com>
2017-01-13 17:40:37 +0200

    Function anyNA using in residual_est

--- vardpoor/R/residual_est.R
+++ vardpoor/R/residual_est.R
@@ -3,35 +3,33 @@ residual_est <- function (Y, X, weight, q) {
  
   # Y
   Y <- data.table(Y, check.names = TRUE)
+  if(anyNA(Y)) print("'Residual_est': 'Ys' has missing values", call. = FALSE)
   n <- nrow(Y)
   m <- ncol(Y)
   if (!all(sapply(Y, is.numeric))) stop("'Y' must be numeric")
-  if(any(is.na(Y))) print("'Residual_est': 'Ys' has missing values", call. = FALSE)
   Y <- as.data.frame.matrix(Y)
  
   # X
-  X <- data.table(X, check.names = TRUE)
+  X <- as.matrix(X, check.names = TRUE)
+  if(anyNA(X)) stop("'X' has missing values")
   if (nrow(X) != n) stop("'X' and 'Y' must be equal row count")
-  if (!all(sapply(X, is.numeric))) stop("'X' must be numeric")
-  X1 <- X[, lapply(.SD, function(x) sum(!is.na(x)))]
-  if (!all(X1 == n)) stop("X has missing values")
-  X <- as.matrix(X)
+  if (is.numeric(class(X[, 1]))) stop("'X' must be numeric")
 
   # weight
   weight <- data.frame(weight)
+  if(anyNA(weight)) stop("'weight' has missing values")
   if (nrow(weight) != n) stop("'weight' length must be equal with 'Y' row count!")
   if (ncol(weight) != 1) stop("'weight' must be a vector or 1 column data.frame, matrix, data.table")
   weight <- weight[, 1]
   if (!is.numeric(weight)) stop("'weight' must be numeric")
-  if(any(is.na(weight))) stop("'weight' has missing values")
 
   # q
   q <- data.frame(q)
+  if(anyNA(q)) stop("'q' has missing values")
   if (nrow(q) != n) stop("'q' length must be equal with Y' row count!")
   if (ncol(q) != 1) stop("'q' must be a vector or 1 column data.frame, matrix, data.table")
   q <- q[, 1]
   if (!is.numeric(q)) stop("'q' must be numeric")
-  if(any(is.na(q))) stop("'q' has missing values")  
 
   ee <- as.data.frame(matrix(NA, n, m))
   ws <- weight * q

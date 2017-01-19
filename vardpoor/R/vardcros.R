0b55be54fbb50a3be381193de613fb8d9d5a6cb4
juris.breidaks@gmail.com <juris.breidaks@gmail.com>
2017-01-19 17:20:19 +0200

    Bug correction

--- vardpoor/R/vardcros.R
+++ vardpoor/R/vardcros.R
@@ -479,8 +479,6 @@ vardcros <- function(Y, H, PSU, w_final,
          if (outp_res) res_outp <- DTc[, c(names(ID_level1h), names_PSU, "w_final", namesY2w), with = FALSE]
      } else DTc[, (namesY2w) := .SD[, namesY2, with = FALSE]]
 
-   DTc[, .SD[, namesY2w, with = FALSE] * get("w_final")]
-
    DTc[, (namesY2w) := .SD[, namesY2w, with = FALSE] * get("w_final")]
 
    #--------------------------------------------------------*

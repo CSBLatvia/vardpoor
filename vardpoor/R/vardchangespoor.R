d421144fff44b9167244f48727b8e364bf036282
Anthony Damico <ajdamico@gmail.com>
2017-02-06 05:56:55 -0500

    if the first record is female, unique() will return c(2,1) and incorrectly stop() here

--- vardpoor/R/vardchangespoor.R
+++ vardpoor/R/vardchangespoor.R
@@ -242,7 +242,7 @@ vardchangespoor <- function(Y, age = NULL,
       gender <- gender[, 1]
       if (!is.numeric(gender)) stop("'gender' must be numeric")
       if (length(unique(gender)) != 2) stop("'gender' must be exactly two values")
-      if (!all.equal(unique(gender), c(1, 2))) stop("'gender' must be value 1 for male, 2 for females")
+      if (!all.equal(sort(unique(gender)), c(1, 2))) stop("'gender' must be value 1 for male, 2 for females")
    } else if (any(type == "lingpg")) stop("'gender' must be numeric")
 
   # sort

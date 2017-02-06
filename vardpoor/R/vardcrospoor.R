d5662f606c6ae3b19e7de5410ce456ae865bb681
juris.breidaks@gmail.com <juris.breidaks@gmail.com>
2017-02-06 12:19:31 +0200

    vardcrospoor bug correction

--- vardpoor/R/vardcrospoor.R
+++ vardpoor/R/vardcrospoor.R
@@ -239,7 +239,7 @@ vardcrospoor <- function(Y, age = NULL, pl085 = NULL,
       if (!is.numeric(gender)) stop("'gender' must be numeric")
       if (length(unique(gender)) != 2) stop("'gender' must be exactly two values")
       if (!all.equal(unique(gender), c(1, 2))) stop("'gender' must be value 1 for male, 2 for females")
-  } if (any(type == "lingpg")) stop("'gender' must be numeric")
+  } else if (any(type == "lingpg")) stop("'gender' must be numeric")
 
   # sort
   if (!is.null(sort)) {
